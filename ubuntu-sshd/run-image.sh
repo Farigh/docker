31763a0246f04b090e48feec618de589209ce508
GARCIN David <david.garcin.pro@gmail.com>
2016-10-02 16:33:50 +0200

    ubuntu-sshd: run-image.sh: Now default to the DockerHub image
    Added a -d option to use the image generated using the build.sh script

--- ubuntu-sshd/run-image.sh
+++ ubuntu-sshd/run-image.sh
@@ -4,11 +4,19 @@ resolved_script_path=`readlink -f $0`
 current_script_dir=`dirname $resolved_script_path`
 current_full_path=`readlink -e $current_script_dir`
 
-option=$1
-docker_image_name=ubuntu-sshd
+# TODO: use getopts
+option1=$1
+option2=$2
+docker_image_name=farigh/minimalist-sshd
 docker_name=ubuntu_sshd_docker
 docker_home_dir="$current_full_path/home"
 host_port=24042
+
+# Devel mode, use local image
+if [ "$option1" == "-d" ] || [ "$option2" == "-d" ]; then
+	docker_image_name=ubuntu-sshd
+fi
+
 # NET_ADMIN capability needed by iptables
 docker_run_opt="--cap-add=NET_ADMIN -d -p ${host_port}:22 --name ${docker_name} ${docker_image_name}"
 
@@ -16,7 +24,7 @@ RESET_COLOR=$(echo -en '\e[00m')
 RED_COLOR=$(echo -en '\e[0;31m')
 CYAN_COLOR=$(echo -en '\e[0;36m')
 
-# Add volume mounting option if dir exixsts
+# Add volume mounting option if dir exists
 if [ -d "$docker_home_dir" ]; then
     docker_run_opt="-v ${docker_home_dir}:/home/sshuser ${docker_run_opt}"
     echo "${CYAN_COLOR}Info: mounting dir '$docker_home_dir' as container's user home dir${RESET_COLOR}"
@@ -25,7 +33,7 @@ fi
 docker_ps=`docker ps -a | grep $docker_name`
 if [ "$docker_ps" == "" ]; then
     docker run $docker_run_opt
-elif [ "$option" == "-f" ]; then
+elif [ "$option1" == "-f" ] || [ "$option2" == "-f" ]; then
    if [ "$(docker inspect -f {{.State.Running}} ${docker_name})" == "true" ]; then
        docker stop $docker_name
    fi
